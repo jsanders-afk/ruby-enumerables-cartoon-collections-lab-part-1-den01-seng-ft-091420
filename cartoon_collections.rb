23c27804bb13f56601b79528672b6685fc8a1a44
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-04 05:48:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,9 +11,6 @@ end
 def list_dwarves(array)
   array.each_with_index do |char|
     puts char
-  # Use `each_with_index` to enumerate over the provided array
-  #
-  # Print a numbered list of each element
   end
 end
 
