af91e46ac4f8c30e38037635d9dff7ddb0b3f147
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-04 05:51:56 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,19 @@
+require 'pry'
+
+array = ["Gimli", "Thorin", "Balin", "Gloin"]
+
 def greet_characters(array)
-  # Use `each` to enumerate over the provided array
-  #
-  # Print a custom greeting for each element
+array.each do |char|
+  puts "Hello #{char}!"
+  end
 end
 
 def list_dwarves(array)
-  # Use `each_with_index` to enumerate over the provided array
-  #
-  # Print a numbered list of each element
+  array.each_with_index do |char, i|
+    puts "#{i + 1}. #{char}"
+  end
 end
+
+
+
+
