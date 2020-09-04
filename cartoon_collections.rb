50e3d89d7c270b22ab4c1e461adb077fa258a570
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-04 05:39:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,10 @@
+require 'pry'
+
+
+
 def greet_characters(array)
-  # Use `each` to enumerate over the provided array
-  #
-  # Print a custom greeting for each element
+ 
+
 end
 
 def list_dwarves(array)
@@ -9,3 +12,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
 end
+
+
+
+
